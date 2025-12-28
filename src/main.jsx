59486462b741757f9a95c952d11aac7d252f2134
Alik Agarwala <alikagarwala@outlook.com>
2025-12-28 19:29:31 +0530

    feat: integrate Clerk for authentication and update routing in the application

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import SignInPage from './auth/sign-in/index.jsx'  
-import { Home } from 'lucide-react';
+import Home from './home/index.jsx';
 import Dashboard from './dashboard/index.jsx';
+import { ClerkProvider } from '@clerk/clerk-react';
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 const router=createBrowserRouter([
   {
@@ -34,6 +38,8 @@ const router=createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router}/>
+    </ClerkProvider>
   </StrictMode>,
 )
